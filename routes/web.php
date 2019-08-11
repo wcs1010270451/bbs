cb74b4ee6c58232d9d1326f99c859da214b874d7
wcs1010270451 <296952503@163.com>
2019-08-11 17:45:06 +0800

    用户可以上传话题图片

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,5 @@ Auth::routes(['verify' => true]);
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
 Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);
+
+Route::post('upload_image', 'TopicsController@uploadImage')->name('topics.upload_image');

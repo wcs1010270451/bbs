647eccc621a2af19720766261d644bb96230e67c
wcs1010270451 <296952503@163.com>
2019-08-11 14:55:07 +0800

    分类话题列表

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ Route::get('/', 'PagesController@root')->name('root');
 Auth::routes(['verify' => true]);
 
 Route::resource('users', 'UsersController', ['only' => ['show', 'update', 'edit']]);
-
+Route::resource('categories', 'CategoriesController', ['only' => ['show']]);
 Route::resource('topics', 'TopicsController', ['only' => ['index', 'show', 'create', 'store', 'update', 'edit', 'destroy']]);

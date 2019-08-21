2c283676b881b6f3ec0a6fa2bf7c4562807b3c36
wcs1010270451 <296952503@163.com>
2019-08-21 16:15:00 +0000

    删除评论

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -87,4 +87,13 @@ class Topic extends Model
     {
         return route('topics.show', array_merge([$this->id, $this->slug], $params));
     }
+
+    /**
+     * 修改回复数
+     */
+    public function updateReplyCount()
+    {
+        $this->reply_count = $this->replies->count();
+        $this->save();
+    }
 }
